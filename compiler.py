3b2a8ee7d6f9196165832d775fffc1b1ebb53cfb
Naveen Bandarage <thebanterage@gmail.com>
2020-12-23 18:00:20 +1300

    added elif support

--- compiler.py
+++ compiler.py
@@ -8,68 +8,79 @@ def sl_compile(sl_input):
     global line
 
     slatt_file = sl_input
-
+    Cartinese = True
     #get line list
     with open(slatt_file) as slatt:
         lineList = slatt.readlines()
 
     #parse the slatt file. 
     with open(slatt_file) as slatt:
+        # if(cartinese == True):
+        #     tempLine = ""
+        #     for num, line in enumerate(slatt, 1):
+                
 
-        for num, line in enumerate(slatt, 1):
-            
-            #search for "ok!" at end of file
-            if lineList[len(lineList) - 1].split()[0] != "ok!" or num == 1 and "**slatt!**" not in line:
-                if lineList[len(lineList) - 1].split()[0] != "ok!":
-                    print('Error in {0}: Non-valid slatt file (error 1: require "ok!" on last line of program file)'.format(slatt_file))
-                    #search for "*slatt!*" at beginning of file
-                if line.split()[0] != "**slatt!**":
-                    print('Error in {0}: Non-valid slatt file (error 2: require "**slatt!**" on line 1 of program file)'.format(slatt_file))
-            else:
-                line = line.replace("**slatt!**", "")
-                line = line.replace("ok!", "")
-            #need to end up adding more actions. 
-            #function to parse slatt file and compile to python\
-            def parse():
-                global slatt_file
-                global line
-                #prints if "+yo pierre()" is in the line
-                slatt_file = slatt_file.replace("/", "")
-                if "+yo pierre(" in line:
-                    line = line.replace("+yo pierre", "print")
-                    parse()
-                #set variables using "^wholelotta>"
-                elif "^wholelotta>" in line:
-                    line = line.replace("^wholelotta>", "=")
-                    parse()
-                #is equal to (==)
-                elif "^iswholelotta>" in line:
-                    line = line.replace("^iswholelotta>", "==")
-                    parse()
-                #define functions using "_flex*"
-                elif "_flex*" in line:
-                    line = line.replace("_flex*", "def ")
-                    parse()
-                #call function
-                elif "_imightcall" in line:
-                    line = line.replace("_imightcall ", "")
-                    parse()
-                #if/else statement
-                elif "*!yah" in line:
-                    line = line.replace("*!yah", "if")
-                    parse()
-                elif "*!nah" in line:
-                    line = line.replace("*!nah", "else")
-                    parse()
-                #input statement
-                elif "wokeuplike*(" in line:
-                    line = line.replace("wokeuplike*", "input")
-                    parse()
+
+
+        # else:
+
+            for num, line in enumerate(slatt, 1):
+                
+                #search for "ok!" at end of file
+                if lineList[len(lineList) - 1].split()[0] != "ok!" or num == 1 and "**slatt!**" not in line:
+                    if lineList[len(lineList) - 1].split()[0] != "ok!":
+                        print('Error in {0}: Non-valid slatt file (error 1: require "ok!" on last line of program file)'.format(slatt_file))
+                        #search for "*slatt!*" at beginning of file
+                    if line.split()[0] != "**slatt!**":
+                        print('Error in {0}: Non-valid slatt file (error 2: require "**slatt!**" on line 1 of program file)'.format(slatt_file))
                 else:
-                    os.system("echo '{0}' >> /tmp/{1}.py".format(line, slatt_file))
-  
-            parse()
+                    line = line.replace("**slatt!**", "")
+                    line = line.replace("ok!", "")
+                #need to end up adding more actions. 
+                #function to parse slatt file and compile to python\
+                def parse():
+                    global slatt_file
+                    global line
+                    #prints if "+yo pierre()" is in the line
+                    slatt_file = slatt_file.replace("/", "")
+                    if "+yo pierre(" in line:
+                        line = line.replace("+yo pierre", "print")
+                        parse()
+                    #set variables using "^wholelotta>"
+                    elif "^wholelotta>" in line:
+                        line = line.replace("^wholelotta>", "=")
+                        parse()
+                    #is equal to (==)
+                    elif "^iswholelotta>" in line:
+                        line = line.replace("^iswholelotta>", "==")
+                        parse()
+                    #define functions using "_flex*"
+                    elif "_flex*" in line:
+                        line = line.replace("_flex*", "def ")
+                        parse()
+                    #call function
+                    elif "_imightcall" in line:
+                        line = line.replace("_imightcall ", "")
+                        parse()
+                    #if/else statement
+                    elif "*!yah" in line:
+                        line = line.replace("*!yah", "if")
+                        parse()
+                    elif "*!nah" in line:
+                        line = line.replace("*!nah", "else")
+                        parse()
+                    elif "*!bih" in line:
+                        line = line.replace("*!bih", "elif")
+                        parse()
+                    #input statement
+                    elif "wokeuplike*(" in line:
+                        line = line.replace("wokeuplike*", "input")
+                        parse()
+                    else:
+                        os.system("echo '{0}' >> /tmp/{1}.py".format(line, slatt_file))
     
+                parse()
+        
     print("""\n//.............................................................................................................................................
 //.................................................................OOOOO...................CCCCC...............................................
 //.PPPPPPPPPPP.PLLLL..........AAAAA..AAYYY...YYYYYYYBBBBBBBB.....OOOOOOOOO..OOOII........CCCCCCCC......AAAAAA....AARRRRRRRRR.RRRTTTTTTTTTTIII..
@@ -94,6 +105,6 @@ def sl_compile(sl_input):
     
     for word in templist:
         print(word)
-
+    print('\n')
     os.system("python /tmp/{0}.py".format(slatt_file))
     os.remove("/tmp/{0}.py".format(slatt_file))
