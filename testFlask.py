822ded796dd17e4d47d4d0f43571afdc6bd4ffe4
Naveen Bandarage <thebanterage@gmail.com>
2021-01-11 17:54:01 +1300

    Made progress to the compiler and design.

--- testFlask.py
+++ testFlask.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from livereload import server
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def my_form():
+    return render_template('my-form.html')
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    text = request.form['textbox']
+    processed_text = text.upper()
+    return processed_text
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    server = Server(app.wsgi_app)
+    server.serve()
+
 
 #export FLASK_APP=testFlask.py
 #flask run
